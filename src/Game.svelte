ede0e8b23f6bea59f878d400ff22da9188215522
Joshua Jenner <33088839+joshuajenner@users.noreply.github.com>
2021-02-17 16:08:36 +0800

    board now displays data from server

--- src/Game.svelte
+++ src/Game.svelte
@@ -1,27 +1,95 @@
 <script>
-  import { room } from "./store.js";
-  import { socket } from "./store.js";
+	import { room } from "./store.js";
+	import { socket } from "./store.js";
 
-  function leaveRoom() {
-    $socket.emit("leaveroom", $room);
-    room.set(undefined);
-  }
+	let board = [];
+
+	function leaveRoom() {
+		$socket.emit("leaveroom", $room);
+		room.set(undefined);
+	}
+
+	async function initBoard() {
+		const res = await fetch("http://127.0.0.1:3000/getboard", {
+			method: "POST",
+			body: JSON.stringify({
+				room: $room,
+			}),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+		const board = await res.json();
+		console.log(board);
+		return board;
+	}
+	board = initBoard();
 </script>
 
-<style>
-  #roomInfo {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-</style>
-
 <div id="roomInfo">
-  <h3>Room Code: {$room}</h3>
-  <div>
-    <button on:click={leaveRoom}>Leave</button>
-  </div>
+	<h3>Room Code: {$room}</h3>
+	<div>
+		<button on:click={leaveRoom}>Leave</button>
+	</div>
 </div>
 <div>
-  <p>Your turn</p>
+	<p>Your turn</p>
+	<div id="board">
+		{#await board}
+			<h3>Loading Board...</h3>
+		{:then b}
+			<div id="boardGrid">
+				{#each b as tile}
+					<div class="tile">
+						<div class="tileContent">{tile}</div>
+					</div>
+				{/each}
+			</div>
+		{/await}
+	</div>
 </div>
+
+<style>
+	#roomInfo {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+	#boardGrid {
+		display: grid;
+		grid-template-columns: auto auto auto;
+		grid-template-rows: auto auto auto;
+		border: 2px solid gray;
+	}
+	.tile {
+		position: relative;
+		border: 1px dashed lightgray;
+	}
+	.tile:nth-child(1),
+	.tile:nth-child(2),
+	.tile:nth-child(3) {
+		border-top: none;
+	}
+	.tile:nth-child(7),
+	.tile:nth-child(8),
+	.tile:nth-child(9) {
+		border-bottom: none;
+	}
+	.tile:nth-child(3n + 1) {
+		border-left: none;
+	}
+	.tile:nth-child(3n) {
+		border-right: none;
+	}
+
+	.tile:after {
+		content: "";
+		display: block;
+		padding-bottom: 100%;
+	}
+	.tileContent {
+		position: absolute;
+		bottom: 50%;
+		left: 50%;
+	}
+</style>
--- src/components/RoomList.svelte
+++ src/components/RoomList.svelte
@@ -1,45 +1,87 @@
 <script>
-  import { room } from "../store.js";
-  import { socket } from "../store.js";
+	import { room } from "../store.js";
+	import { socket } from "../store.js";
 
-  let check = 0;
-  let createdRoom = "";
+	let check = 0;
+	let createdRoom = "";
+	let listedRooms = [];
 
-  $socket.on("updaterooms", arg => {
-    console.log(arg);
-  });
+	function createRoom() {
+		while (check == 0) {
+			check = Math.random();
+		}
+		createdRoom = check.toString(36).substr(2, 5);
+		joinRoom(createdRoom);
+		check = 0;
+	}
 
-  function createRoom() {
-    while (check == 0) {
-      check = Math.random();
-    }
-    createdRoom = check.toString(36).substr(2, 5);
-    joinRoom(createdRoom);
-    check = 0;
-  }
+	function joinRoom(givenRoom) {
+		$socket.emit("joinroom", givenRoom);
+		room.set(givenRoom);
+	}
+	async function listRooms() {
+		const res = await fetch("http://127.0.0.1:3000/getrooms", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+		const loginjson = await res.json();
+		console.log(loginjson);
+		return loginjson;
+	}
+	listedRooms = listRooms();
 
-  function joinRoom(givenRoom) {
-    $socket.emit("joinroom", givenRoom);
-    room.set(givenRoom);
-  }
-  function listRooms() {
-    $socket.emit("listrooms");
-  }
-  listRooms();
+	$socket.on("updaterooms", (arg) => {
+		console.log(arg);
+		listedRooms = arg;
+	});
 </script>
 
-<style>
-  #listHeader {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-</style>
-
 <div id="listHeader">
-  <h1>All Rooms</h1>
-  <div>
-    <button on:click={createRoom}>Create</button>
-  </div>
-
+	<h1>All Rooms</h1>
+	<div>
+		<button on:click={createRoom}>Create</button>
+	</div>
 </div>
+
+<div id="allRooms">
+	{#await listedRooms}
+		<h3>Loading...</h3>
+	{:then rooms}
+		{#if rooms.length > 0}
+			{#each rooms as room}
+				<div class="roomItem">
+					<div class="roomNum">{room[0]}</div>
+					<div class="roomData">
+						<div class="numJoined">{room[1].length}/2</div>
+						<div class="roomJoin">
+							{#if room[1].length > 1}
+								<button on:click={joinRoom(room[0])}>View</button>
+							{:else}
+								<button on:click={joinRoom(room[0])}>Play</button>
+							{/if}
+						</div>
+					</div>
+				</div>
+			{/each}
+		{:else}
+			<h3>No rooms found. Please click above to create one.</h3>
+		{/if}
+	{:catch error}
+		<h3>Error</h3>
+	{/await}
+</div>
+
+<style>
+	#listHeader,
+	.roomItem {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+	.roomData {
+		display: flex;
+		align-items: center;
+	}
+</style>
